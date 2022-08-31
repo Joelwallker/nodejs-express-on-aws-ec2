8d5afb2dd2e9954f859234376d3c88e746fcd27a
Joelwallker <cominghome120@gmail.com>
2022-08-31 10:41:36 +0300

    url update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #download node and npm 
-curl -o- https://raw.githubusercontent.com/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
